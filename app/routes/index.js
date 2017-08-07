2be7e0fc0eff2bb3bec46f9ff63fe1b10e9f2919
Joel Bakken <joel@>
2017-08-07 13:12:08 -0700

    lesson follow along components update

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,15 +6,30 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    saveRental3(oarams) {
+    saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
     },
-    
+
+    update(rental, params) {
+      debugger;
+      rental.save();
+      this.transitionTo('index');
+    },
+
     destroyRental(rental) {
       rental.destroyRecord();
       this.transitionTo('index');
     }
+    update(rental, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          rental.set(key,params[key]);
+        }
+      });
+      rental.save();
+      this.transitionTo('index');
+    },    
   }
 });
